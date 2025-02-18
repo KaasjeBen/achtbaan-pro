149196155df276bf90af822a1477c504b85ad3f3
Ben <D310581@edu.curio.nl>
2025-02-18 14:11:49 +0100

    kaas

---
+++ Achtbaan/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="icon" href="<?php echo $base_url; ?>images/favicon.ico" type="image/x-icon">
+    <title>Vallende Duivel</title>
+</head>
+<body>
+    
+</body>
+</html>
